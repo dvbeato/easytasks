bf618121d503549635e6a1d5713663e31ade42ef
dvbeato <diogo.vecchiati@gmail.com>
2015-12-22 18:57:40 -0200

    setup devise support for rspec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
-  factory :user do
-    
+  factory :account do
+    email { Faker::Internet.email }
+    password "password"
+    password_confirmation "password"
   end
-
 end
