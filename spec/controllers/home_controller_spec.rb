795e327981a0b9fe6841145f7571d55aea124926
dvbeato <diogo.vecchiati@gmail.com>
2015-12-25 17:42:34 -0200

    fix spec for authenticated user access home index controller

--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -2,10 +2,13 @@ require 'rails_helper'
 
 RSpec.describe HomeController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  context "when a authenticated user" do
+    login_user 
+    describe "GET #index" do
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
     end
   end
 
