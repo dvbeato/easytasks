d5cc94c264419a3976421cd5f3f4eaf6f7d309b4
dvbeato <diogo.vecchiati@gmail.com>
2015-12-28 16:40:35 -0200

    create helper to check user is current user

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def avatar(user, size=48)
     "#{user.avatar_url}?s=#{size}&d=mm"
   end
+
+  def current_user?(user)
+    user.id == current_user.id
+  end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -15,4 +15,23 @@ RSpec.describe UsersHelper, type: :helper do
     end
   end
   
+  describe "current_user?" do
+    
+    let(:user) { create(:user) }
+    let(:current_user) { create(:user) }
+
+    before do 
+      allow(helper).to receive(:current_user).and_return(current_user) 
+    end
+
+    it "returns true when is current_user" do
+      expect(helper.current_user?(current_user)).to be_truthy
+    end
+
+    it "returns false when is not current_user" do
+      expect(helper.current_user?(user)).to be_falsey
+    end
+
+  end
+
 end
