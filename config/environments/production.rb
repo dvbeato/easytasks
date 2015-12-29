9fd03ac29e88db2cd9e3a49c2fd7e6737479c2aa
dvbeato <diogo.vecchiati@gmail.com>
2015-12-29 09:28:50 -0200

    setup devise mailer and mandrill

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Mail settings
+  config.action_mailer.default_url_options = { :host => 'easytasks.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address   => "smtp.mandrillapp.com",
+    :port      => 587, 
+    :enable_starttls_auto => true, 
+    :user_name => "no-reply@easytasks.com",
+    :password  => ENV["EASYTASK_MANDRILL_KEY"], 
+    :authentication => 'plain', 
+    :domain => 'easytasks.herokuapp.com', 
+  }
 end
