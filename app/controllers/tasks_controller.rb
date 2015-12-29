bcb5934bada1589e595ea26b0af699ce9bd2c8ae
dvbeato <diogo.vecchiati@gmail.com>
2015-12-29 11:26:56 -0200

    implement ajax task creation and destroy

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,23 +3,18 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:update, :destroy]
 
   before_action :authenticate_user!
-  
-  respond_to :js
 
   def create
     @task = @task_list.tasks.build(task_params)
     @task.save
-    render :show, status: :created
   end
 
   def update
     @task.update(task_params)
-    render :show, status: :updated
   end
 
   def destroy
     @task.destroy
-    render :destroy
   end
 
   private
