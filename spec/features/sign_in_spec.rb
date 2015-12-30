63cb12d2c406a3c02ae9b42fc36c0e1348d4100e
dvbeato <diogo.vecchiati@gmail.com>
2015-12-30 10:16:22 -0200

    refactor sign in spec to have more expressive message

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -2,16 +2,20 @@ RSpec.feature 'User Log in' do
 
   given!(:user) { create(:user) }
 
-  scenario 'with valid email and password' do
-    sign_in user.email, user.password
+  context "with valid email and password" do
+    scenario "redirects to home page" do
+      sign_in user.email, user.password
 
-    expect(current_path).to eq(root_path)
+      expect(current_path).to eq(root_path)
+    end
   end
 
-  scenario 'with invalid email and password' do
-    sign_in "invalid_user", "invalid_password"
-  
-    expect(current_path).to eq(new_user_session_path)
+  context "with invalid email and password" do
+    scenario "redirects to login page" do
+      sign_in "invalid_user", "invalid_password"
+    
+      expect(current_path).to eq(new_user_session_path)
+    end
   end
 
 end
